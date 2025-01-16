806af616209ab630344dc910a860071750391873
bexruz-cyber <mayoq2024@gmail.com>
2025-01-16 19:20:23 +0500

    add

--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,59 @@
-let closeBtn = document.querySelector('#close')
-let form = document.querySelector('.modal')
+    const modal = document.querySelector('.modal');
+    const closeButton = document.getElementById('close');
+    const video = document.querySelector('iframe');
+    let intervalId = null;
+    let logIntervalId = null;
 
-closeBtn.addEventListener('click' , ()=>{
-    form.classList.add("hidden")
-    form.classList.remove("flex")
-})
-setTimeout(function () {
-    form.classList.remove('hidden')
-    form.classList.add('flex')
+    const openModal = () => {
+        modal.classList.remove('hidden');
+    };
 
-    setInterval(() => {
-        form.classList.remove('hidden')
-        form.classList.add('flex')
-    }, 120000);
-}, 5000)
+    const closeModal = () => {
+        modal.classList.add('hidden');
+    };
+
+    closeButton.addEventListener('click', closeModal);
+
+
+
+    const logVideoTime = (player) => {
+        logIntervalId = setInterval(() => {
+            const currentTime = player.getCurrentTime();
+            console.log(`Current video time: ${currentTime} seconds`);
+        }, 1000); // Har soniyada vaqtni konsolga chiqarish
+    };
+
+    const onYouTubeIframeAPIReady = () => {
+        const player = new YT.Player(video, {
+            events: {
+                'onReady': () => {
+                    logVideoTime(player); // Video tayyor bo'lganda vaqtni logga chiqarishni boshlash
+                },
+                'onStateChange': (event) => {
+                    if (event.data === YT.PlayerState.PLAYING) {
+                        console.log("succes");
+
+                        if (!intervalId) {
+                            startModalTimer();
+                        }
+                    } else if (event.data === YT.PlayerState.PAUSED || event.data === YT.PlayerState.ENDED) {
+                        clearInterval(intervalId);
+                        clearInterval(logIntervalId); // Log intervalni to'xtatish
+                        intervalId = null;
+                        logIntervalId = null;
+                    }
+                }
+            }
+        });
+    };
+
+    const tag = document.createElement('script');
+    tag.src = "https://www.youtube.com/iframe_api";
+    document.body.appendChild(tag);
+    window.onYouTubeIframeAPIReady = onYouTubeIframeAPIReady;
+
+const startModalTimer = () => {
+    intervalId = setInterval(() => {
+        openModal();
+    }, 300); // 300000 ms = 5 daqiqa
+};
