86a418af1268f98a32d35a0ff2beaa086adba98a
abdulvahob0714@gmail.com <dilbarxonxiziraliyeva@gmail.com>
2025-01-17 10:40:10 +0500

    add

--- public/js/app.js
+++ public/js/app.js
@@ -5,63 +5,16 @@ btn.addEventListener('click' , function(){
     modal.classList.remove("hidden")
     // modal.classList.add("flex")
 })
-    const closeButton = document.getElementById('close');
-    const video = document.querySelector('iframe');
-    let intervalId = null;
-    let logIntervalId = null;
+   
 
-    const openModal = () => {
-        modal.classList.remove('hidden');
-    };
+setTimeout(function(item , index){
+    modal.classList.remove("hidden")
+} , 2000)
 
-    const closeModal = () => {
-        modal.classList.add('hidden');
-    };
+setTimeout(function(item, index) {
+    modal.classList.remove("hidden");
+}, 300000); // 5 daqiqa = 300,000 ms
 
-    closeButton.addEventListener('click', closeModal);
-
-
-
-    const logVideoTime = (player) => {
-        logIntervalId = setInterval(() => {
-            const currentTime = player.getCurrentTime();
-            console.log(`Current video time: ${currentTime} seconds`);
-        }, 1000); // Har soniyada vaqtni konsolga chiqarish
-    };
-
-    const onYouTubeIframeAPIReady = () => {
-        const player = new YT.Player(video, {
-            events: {
-                'onReady': () => {
-                    logVideoTime(player); // Video tayyor bo'lganda vaqtni logga chiqarishni boshlash
-                },
-                'onStateChange': (event) => {
-                    if (event.data === YT.PlayerState.PLAYING) {
-                        console.log("succes");
-
-                        if (!intervalId) {
-                            startModalTimer();
-                        }
-                    } else if (event.data === YT.PlayerState.PAUSED || event.data === YT.PlayerState.ENDED) {
-                        clearInterval(intervalId);
-                        clearInterval(logIntervalId); // Log intervalni to'xtatish
-                        intervalId = null;
-                        logIntervalId = null;
-                    }
-                }
-            }
-        });
-    };
-
-    const tag = document.createElement('script');
-    tag.src = "https://www.youtube.com/iframe_api";
-    document.body.appendChild(tag);
-    window.onYouTubeIframeAPIReady = onYouTubeIframeAPIReady;
-
-const startModalTimer = () => {
-    intervalId = setInterval(() => {
-        openModal();
-    }, 300); // 300000 ms = 5 daqiqa
-};
-
-
+setTimeout(function(item, index) {
+    modal.classList.remove("hidden");
+}, 600000); // 5 daqiqa = 300,000 ms
